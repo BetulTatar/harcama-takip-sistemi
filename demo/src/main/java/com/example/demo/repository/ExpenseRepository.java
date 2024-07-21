18dc48f3859cbd93b18fdb9f9bbfc95056934ccf
tatarbetul <tatarbetul14@gmail.com>
2024-07-21 23:39:36 +0300

    Error Handeling

--- demo/src/main/java/com/example/demo/repository/ExpenseRepository.java
+++ demo/src/main/java/com/example/demo/repository/ExpenseRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense,Long> {
     @Query("SELECT SUM(e.amount) FROM Expense e WHERE e.user.id = :userId")
     Double findTotalExpenseByUserId(@Param("userId") Long userId);
+    List<Expense> findByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
 }
